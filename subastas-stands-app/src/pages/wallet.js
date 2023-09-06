2ea261483365ae06fef704ec3f79583531ab8bec
Diego Flores <dfmonroy07@gmai.com>
2023-09-06 17:17:20 -0600

    added use state

--- subastas-stands-app/src/pages/wallet.js
+++ subastas-stands-app/src/pages/wallet.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Wallet } from "../components"
 import dummy from "../dummy.json";
 
 function WalletPage() {
     const { user_id } = useParams();
+    const [transactions, setTransactions] = useState({})
+    const [valid_codes, setValidCodes] = useState({})
+    
     return (
         <Wallet name={"Ana María García"} balance={100000} history={dummy} />
     );
