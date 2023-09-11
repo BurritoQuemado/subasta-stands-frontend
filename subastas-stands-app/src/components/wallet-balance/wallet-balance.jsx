05df98769a0cf7aac7f271dc2fe1dcc2d098470f
Diego Flores <dfmonroy07@gmai.com>
2023-09-11 11:41:34 -0600

    change: updated text

--- subastas-stands-app/src/components/wallet-balance/wallet-balance.jsx
+++ subastas-stands-app/src/components/wallet-balance/wallet-balance.jsx
@@ -7,7 +7,7 @@ function WalletBalance({ name, balance}) {
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto max-w-2xl text-center">
                     <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"> { name } tu balance es: </h2>                    
-                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"> ${ balance } polarcoins</h2>                    
+                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"> { balance } polarcoins</h2>                    
                 </div>
             </div>
         </>
