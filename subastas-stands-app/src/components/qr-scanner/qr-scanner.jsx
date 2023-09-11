c30a897e520e7ba0dee25b119128595487180e3f
Diego Flores <dfmonroy07@gmai.com>
2023-09-11 12:07:03 -0600

    feat: change scanning initial state and button placement

--- subastas-stands-app/src/components/qr-scanner/qr-scanner.jsx
+++ subastas-stands-app/src/components/qr-scanner/qr-scanner.jsx
@@ -5,7 +5,7 @@ import { QrReader } from 'react-qr-reader';
 function Scanner ({ transactions, valid_codes, user_id }) {
     const delay = 100000;
     const [scannedCode, setscannedCode] = useState({});
-    const [scanning, setScanning] = useState(true);
+    const [scanning, setScanning] = useState(false);
     const [unregistered, setUnregistered] = useState(false);
     const [alreadyScanned, setAlreadyScanned] = useState(false);
     const url = 'localhost:3000/registerTransaction';
@@ -79,7 +79,17 @@ function Scanner ({ transactions, valid_codes, user_id }) {
 
     return (
         <>
-            <div className='pt-6 pb-6 pr-4 pl-4 bg-gray-200'>
+            <div className="mt-10 flex items-center justify-center py-2 space-x-2">
+                <button 
+                    type="button" 
+                    onClick={() => resetScanner()}
+                    className="flex w-42 items-center justify-center rounded-lg border border-transparent bg-amber-400 px-8 py-2 text-base font-medium text-white md:py-4 md:px-10 md:text-lg"
+                >
+                    Agregar Polarcoins
+                </button>
+            </div>
+           { scanning ? (
+                <div className='pt-6 pb-6 pr-4 pl-4'>
                 { scanning?
                     <QrReader
                     delay={delay}
@@ -92,34 +102,26 @@ function Scanner ({ transactions, valid_codes, user_id }) {
                     }
                     />
                     : null
+                    } 
+                {
+                    unregistered?
+                        <div className='justify-center mt-6'>
+                            <h1 className='text-center'>Codigo QR Invalido</h1>
+                        </div>
+                        :   null
                 } 
-               {
-                unregistered?
-                    <div className='justify-center mt-6'>
-                        <h1 className='text-center'>Codigo QR Invalido</h1>
-                    </div>
-                    :   null
-               } 
-               {
-                alreadyScanned ?
-                    <div className='justify-center mt-6'>
-                        <h2 className='text-center text-bold text-red-500'> Este codigo QR ya ha sido registado </h2>  
-                        <h1 className='text-center'>QR Escaneado</h1>
-                        <h2 className='text-center'>Recompensa por:  {scannedCode.description}</h2>
-                        <h2 className='text-center'>Creditos obtenidos: {scannedCode.value} </h2>
-                    </div>
-                    : null
-               }
-            </div>
-            <div className="mt-10 flex items-center justify-center py-2 space-x-2">
-                <button 
-                    type="button" 
-                    onClick={() => resetScanner()}
-                    className="flex w-42 items-center justify-center rounded-lg border border-transparent bg-amber-400 px-8 py-2 text-base font-medium text-white md:py-4 md:px-10 md:text-lg"
-                >
-                    Escanear otro QR
-                </button>
-            </div>
+                {
+                    alreadyScanned ?
+                        <div className='justify-center mt-6'>
+                            <h2 className='text-center text-bold text-red-500'> Este codigo QR ya ha sido registado </h2>  
+                            <h1 className='text-center'>QR Escaneado</h1>
+                            <h2 className='text-center'>Recompensa por:  {scannedCode.description}</h2>
+                            <h2 className='text-center'>Creditos obtenidos: {scannedCode.value} </h2>
+                        </div>
+                        : null
+                }
+                </div>
+            ): null}  
         </>
     );
 
