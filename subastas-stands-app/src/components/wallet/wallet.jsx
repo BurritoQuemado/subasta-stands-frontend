1f0b55736bf157b1dd046d06c479940d2b6ad742
Diego Flores <dfmonroy07@gmai.com>
2023-09-11 13:18:38 -0600

    fix: fixed sizes and order of components

--- subastas-stands-app/src/components/wallet/wallet.jsx
+++ subastas-stands-app/src/components/wallet/wallet.jsx
@@ -10,27 +10,26 @@ import {
 function Wallet({ name, balance, history}){
     return (
         <>
-            <div className="bg-white py-24 sm:py-32">
-                    <Balance name={name} balance={balance} /> 
-                <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-1">
-                    <Transactions history_list={history} />
-                    <QRScanner />
-                    <div className="mt-10 flex items-center justify-center gap-x-6">
-                        <Link
-                        to="/billetera/1"
-                        className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        >
-                            Agregar polarcoins
-                        </Link>
-                        <Link
-                            to="https://8th.io/hb8db"
-                            target="_blank"
-                            rel="nonreferrer noreferrer"
-                            className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
-                            >
-                            Experiencia AR
-                        </Link>
+            <div className="bg-white py-10 lg:py-24 sm:py-32">
+                <Balance name={name} balance={balance} />
+                <div className='mt-4 lg:mt-12'>
+                    <div className="text-center">
+                        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"> Historial de transacciones: </h2>                                      
                     </div>
+                    <div className="mx-auto mt-2 lg:mt-0 grid max-w-2xl auto-rows-fr grid-cols-1 gap-0 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-1">
+                        <Transactions history_list={history} />
+                    </div>
+                </div>
+                <QRScanner />
+                <div className="mt-2 flex items-center justify-center">
+                    <Link
+                        to="https://8th.io/hb8db"
+                        target="_blank"
+                        rel="nonreferrer noreferrer"
+                        className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+                        >
+                        Experiencia AR
+                    </Link>
                 </div>
             </div>
         </>
