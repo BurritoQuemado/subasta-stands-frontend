b95499dc6da61399c044a6fbc9e87e77eac3261b
Diego Flores <dfmonroy07@gmail.com>
2023-09-17 18:24:12 -0600

    added use effect

--- subastas-stands-app/src/pages/wallet.js
+++ subastas-stands-app/src/pages/wallet.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import axios from "axios";
+import { json, useParams } from "react-router-dom";
 import { Wallet } from "../components"
 import dummy from "../dummy.json";
 
@@ -7,6 +8,29 @@ function WalletPage() {
     const { user_id } = useParams();
     const [transactions, setTransactions] = useState({})
     const [valid_codes, setValidCodes] = useState({})
+    const URL = "http://localhost:3000/getTransactions"
+    
+    const config = {
+        method: 'get',
+        url: URL,
+        body: JSON.stringify({
+            "user_id": user_id
+        }),
+        headers: { 
+          'Content-Type': 'application/json',
+          "Access-Control-Allow-Origin": "*"
+        },
+    };
+
+    console.log('userid', user_id)
+    useEffect(() => {
+
+        axios.request(config)
+        .then(data => {
+            console.log(data)
+        })
+
+    },[])
     
     return (
         <Wallet name={"Ana María García"} balance={100000} history={dummy} />
