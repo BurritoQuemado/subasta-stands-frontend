685eff0bf8fc377f71940d5a5197d6172d25016d
Diego Flores <dfmonroy07@gmail.com>
2023-09-19 17:30:25 -0600

    changed title

--- subastas-stands-app/src/pages/login.js
+++ subastas-stands-app/src/pages/login.js
@@ -15,7 +15,7 @@ function LoginPage({ setLoggedIn }) {
         <>
             <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-md">
-                    <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Ingresa a tu cartera de Cryptolicoins</h2>
+                    <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Ingresa a la experiencia crypto-artística</h2>
                 </div>
 
                 <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
