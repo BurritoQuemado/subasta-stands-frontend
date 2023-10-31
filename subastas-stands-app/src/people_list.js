3afcfb06deba447961c08a1f03b555fbecc2ab98
Diego Flores <dfmonroy07@gmai.com>
2023-10-31 13:11:11 -0600

    updated list

--- subastas-stands-app/src/people_list.js
+++ subastas-stands-app/src/people_list.js
@@ -1,60 +1,60 @@
-import KarlImg from "./assets/images/people/karl-landsteiner.jpg"
-import HectorElisaImg from './assets/images/people/hectoryelisa.jpg'
-import RobinCoombsImg from './assets/images/people/robin-coombs.jpg'
-import PatriciaTipperImg from './assets/images/people/patriciatippet.jpg'
-import PhilipLevineImg from './assets/images/people/philip-levine.jpg'
-import AlexanderWiennerImg from './assets/images/people/alexander-wiener.jpg'
-import LeonardLandoiseImg from './assets/images/people/leonard-landois.jpg'
-import GregorMendelImg from './assets/images/people/gregor-mendel.jpg'
+import RudolfLudwigImg from "./assets/images/people/rudolf-ludwig.jpg"
+import ElisaYHectorImg from './assets/images/people/elisa-y-hector.jpg'
+import AlexanderReussImg from './assets/images/people/alexander-reuss.jpg'
+import NoelRImg from './assets/images/people/noel-r.jpg'
+import PaulMorawitzImg from './assets/images/people/paul-morawitz.jpg'
+import EvaEngvallImg from './assets/images/people/eva-engvall.jpg'
+import PeterPerlmanImg from './assets/images/people/peter-perlmanjpg'
+import AndrewBuchananImg from './assets/images/people/andrew-buchanan.jpg'
 
 const people_list = [
 {
     "id": 1,
-    "title": "Dr. Karl Landsteiner",
-    "desc": "El Dr. Karl Landsteiner fue un inmunólogo y patólogo austriaco nacido en 1868, conocido principalmente por su descubrimiento del sistema de tipificación sanguínea ABO. Este descubrimiento revolucionario, realizado en 1901, permitió las transfusiones de sangre seguras al identificar la compatibilidad entre diferentes tipos de sangre. Landsteiner también colaboró con otros científicos para descubrir el factor Rh, otro elemento clave en la tipificación sanguínea. Su trabajo en inmunología y medicina le valió el Premio Nobel de Fisiología o Medicina en 1930. Landsteiner también contribuyó al descubrimiento del virus de la poliomielitis",
-    "img": KarlImg
+    "title": "Rudolf Ludwig Karl Virchow",
+    "desc": "Rudolf Virchow fue un médico, patólogo, biólogo y antropólogo alemán del siglo XIX, a menudo llamado el \"padre de la patología moderna\". Nació en 1821 en Schivelbein, Pomerania. Estudió medicina en Berlín y se convirtió en profesor allí. Desarrolló la teoría celular, afirmando que todas las células provienen de células preexistentes. También introdujo conceptos científicos en la autopsia y estableció conexiones entre enfermedades y condiciones sociales. Virchow fue un defensor del progreso social y la educación pública. Fue también político y defensor de la salud pública. Murió en 1902 en Berlín. Su legado perdura en la medicina moderna.",
+    "img": RudolfLudwigImg
 },
 {
     "id": 2,
-    "title": "Héctor Rodríguez Moyado y Elisa Quintanar",
-    "desc": "Héctor Rodríguez Moyado y Elisa Quintanar son figuras notables en el campo de la inmunohematología en México. Rodríguez Moyado fue un hematólogo que contribuyó significativamente al entendimiento y práctica de la inmunohematología, especialmente en áreas como compatibilidad sanguínea y técnicas de transfusión. Elisa Quintanar, por otro lado, también ha hecho contribuciones importantes en la misma disciplina. Juntos, han trabajado para establecer estándares y protocolos que se utilizan en México para garantizar transfusiones seguras y efectivas. Aunque la información detallada puede variar, su legado incluye la formación de profesionales en este campo y la mejora de las prácticas clínicas en transfusión e inmunohematología, teniendo un impacto duradero en la medicina transfusional en México.",
-    "img": HectorElisaImg
+    "title": "Dr. Peter Perlman",
+    "desc": "Dr. Peter Perlmann, originario de Sudetenland, huyó a Suecia durante la invasión nazi, estudió Biología y Química, y co-inventó el test ELISA en 1971 junto a Engvall, revolucionando la detección médica de hormonas y virus.",
+    "img": PeterPerlmanImg
   },
   {
     "id": 3,
-    "title": "Dr. Robin Coombs",
-    "desc": "El Dr. Robin Coombs fue un inmunólogo británico nacido en 1921 y fallecido en 2006. Es más conocido por desarrollar la prueba de Coombs, un examen de laboratorio utilizado para detectar anticuerpos que pueden causar una serie de condiciones, como la anemia hemolítica y reacciones adversas a transfusiones de sangre. Su trabajo revolucionó el campo de la inmunohematología y permitió avances en diagnósticos y tratamientos en medicina transfusional y enfermedades autoinmunes. Recibió numerosos premios y reconocimientos por su contribución a la ciencia médica. Su legado sigue siendo relevante en el ámbito de la medicina moderna.",
-    "img": RobinCoombsImg
+    "title": "Q. Elisa Quintanar García / Dr. Héctor Rodríguez Moyado",
+    "desc": "Elisa Quintanar García, químico farmacobiólogo, lideró el Laboratorio del Banco Central de Sangre en el Centro Médico Nacional Siglo XXI (1962-1997) y fue autora de publicaciones sobre medicina transfusional en México​1​​2​​3​. Dr. Héctor Rodríguez Moyado, hematólogo certificado, dirigió el mismo banco de sangre durante el mismo periodo y publicó investigaciones sobre hematología y medicina transfusional​4​​5​​6​. Ambos coescribieron \"El Banco de Sangre y la Medicina Transfusional\", un libro crucial en su campo​​.",
+    "img": ElisaYHectorImg
   },
   {
     "id": 4,
-    "title": "Patricia Tippet",
-    "desc": "En 1986, Patricia Tippet propuso una teoría avanzada sobre el sistema Rh en la genética humana. Según su teoría, existen dos genes clave: RH D, que codifica el antígeno D, y RH CE, que codifica los antígenos \"C\", \"c\", \"E\", y \"e\". Además, introdujo un tercer gen, RH AG, que produciría una proteína de membrana actuando como sustancia precursora para los antígenos Rh. Solo si el gen RH AG se expresa, entonces los antígenos Rh serán expresados. Estudios posteriores validaron que estas proteínas de transmembrana son codificadas por genes en el cromosoma 1 (RHD y RHCE) y cromosoma 6 (RHAG).",
-    "img": PatriciaTipperImg
+    "title": "Alexander Reuss",
+    "desc": "Alexander Reuss (1838–1887) fue un médico y patólogo alemán conocido por sus contribuciones en el campo de la patología. Estudió medicina y luego se desempeñó en varios puestos académicos y hospitalarios. Uno de sus logros más notables es su trabajo en el campo de la histología, y es recordado por la \"membrana de Reuss\", una capa del oído interno también conocida como la membrana basal del órgano de Corti. Aunque no es tan ampliamente reconocido como algunos de sus contemporáneos, Reuss dejó una marca en el campo de la medicina y su legado continúa siendo estudiado y respetado en la comunidad médica.",
+    "img": AlexanderReussImg
   },
   {
     "id": 5,
-    "title": "Dr. Philip Levine",
-    "desc": "El Dr. Philip Levine es conocido por su investigación pionera en el campo de la inmunohematología, incluida la identificación y comprensión del sistema de grupos sanguíneos Rh. Su trabajo fue fundamental para el desarrollo de pruebas de compatibilidad sanguínea, lo que llevó a avances significativos en transfusiones seguras y el manejo del embarazo Rh-negativo. En colaboración con otros científicos, sus descubrimientos han salvado innumerables vidas y siguen siendo un componente crucial en la medicina transfusional y la obstetricia.",
-    "img": PhilipLevineImg
+    "title": "Dr. Noel R. Rose",
+    "desc": "El Dr. Noel R. Rose (1927-2020), considerado el \"padre de la autoinmunidad\", transformó nuestra comprensión de las enfermedades autoinmunes. Mientras estaba en la Universidad de Buffalo en la década de 1950, demostró que el sistema inmunológico podía atacar a los propios tejidos del cuerpo. Esta revelación cambió radicalmente las percepciones científicas del sistema inmunológico. Durante su carrera, ocupó puestos clave, especialmente en Johns Hopkins University, formando a muchos líderes en inmunología. Su legado en el estudio de la autoinmunidad sigue siendo fundamental hoy.",
+    "img": NoelRImg
   },
   {
     "id": 6,
-    "title": "Dr. Alexander Wiener",
-    "desc": "El Dr. Alexander Wiener fue un serólogo e inmunohematólogo estadounidense que co-descubrió el factor Rh en la sangre humana en 1937, junto con Karl Landsteiner. Este descubrimiento transformó la medicina transfusional y la obstetricia, permitiendo diagnósticos y tratamientos para prevenir la enfermedad hemolítica del recién nacido. Wiener también es reconocido por su trabajo en la clasificación de grupos sanguíneos y su aplicación en medicina forense. Su investigación abrió el camino para la comprensión de la incompatibilidad sanguínea y el desarrollo de pruebas prenatales y tratamientos en casos de incompatibilidad Rh entre madre e hijo.",
-    "img": AlexanderWiennerImg
+    "title": "Dr. Paul Morawitz",
+    "desc": "El Dr. Paul Morawitz (1879-1936) fue un médico y bioquímico alemán famoso por sus contribuciones a la hematología. Es conocido principalmente por su trabajo en la coagulación de la sangre. En 1905, Morawitz propuso una teoría sobre la coagulación sanguínea, describiendo la interacción de cuatro sustancias (hoy conocidas como factor de coagulación) que resulta en la formación de un coágulo. Esta formulación, aunque simplificada en comparación con el entendimiento moderno, sentó las bases para estudios posteriores en la coagulación. Su trabajo ha sido fundamental para el desarrollo de tratamientos y la comprensión de trastornos relacionados con la coagulación.",
+    "img": PaulMorawitzImg
   },
   {
     "id": 7,
-    "title": "Dr. Leonard Landois",
-    "desc": "El Dr. Leonard Landois fue un fisiólogo y médico austríaco del siglo XIX, conocido por su trabajo en hematología. Su libro \"Handbuch der Physiologischen Anatomie des Menschen\" (Manual de Anatomía Fisiológica Humana) se convirtió en un texto de referencia en su época. Landois investigó el sistema circulatorio y la composición de la sangre. Entre sus contribuciones más significativas se encuentra la investigación sobre la coagulación de la sangre y los mecanismos que intervienen en este proceso. Aunque no es tan conocido como otros científicos de su tiempo, su trabajo dejó una huella en la medicina y la fisiología.",
-    "img": LeonardLandoiseImg
+    "title": "Dra. Eva Engvall",
+    "desc": "Dra. Eva Engvall, nacida en Suecia (1940), co-inventó el test ELISA en 1971, esencial en inmunología. Obtuvo su PhD en la Universidad de Estocolmo en 1975, y realizó investigaciones en interacciones moleculares. Posteriormente, trabajó en Sanford-Burnham Medical Research Institute en California y en la Universidad de Estocolmo entre 1993 y 1996.",
+    "img": EvaEngvallImg
   },
   {
     "id": 8,
-    "title": "Gregor Mendel",
-    "desc": "Gregor Mendel fue un monje austriaco y botánico que vivió en el siglo XIX y es considerado el padre de la genética moderna. Realizó experimentos pioneros con plantas de guisante entre 1856 y 1863, estableciendo las bases de la herencia genética. Mendel identificó que ciertos rasgos siguen patrones predecibles de una generación a otra, formulando las leyes fundamentales de la herencia: la ley de la segregación y la ley de la distribución independiente. Aunque su trabajo no fue ampliamente reconocido durante su vida, sus descubrimientos fueron redescubiertos y validados décadas más tarde, sentando las bases para la genética moderna.",
-    "img": GregorMendelImg
+    "title": "Dr. Andrew Buchanan",
+    "desc": "Dr. Andrew Buchanan fue Profesor de Fisiología en la Universidad de Glasgow entre 1839 y 1876, y cirujano en el Glasgow Royal Infirmary de 1835 a 1862​​. Fue el primer Regius Professor de los Institutos de Medicina en la Universidad de Glasgow, plantea el concepto revolucionario de que existe un elemento específico de la  sangre capaz de iniciar la coagulación. Aquí se refiere sin saberlo, al factor tisular. Sus trabajos dan lugar a estudios sobre la coagulación.",
+    "img": AndrewBuchananImg
   }
 ]
 
